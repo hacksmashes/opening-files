054730c518b3d29f43aba9a6f283696cef53dcba
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-02 20:29:07 +0530

    Update open video.py

--- open video.py
+++ open video.py
@@ -1,13 +1,5 @@
 import os
 video=os.startfile("D:\\videos\\Rajini intro.mp4")
 
-
-
-
      # mention the correct directory where u have stored the video in ur system
      # don't forget to write the file type  ( .mp4 )  
-
-
-
-
-
