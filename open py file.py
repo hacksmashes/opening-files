2246b857941ca3c6bbdde9257eba3e07c4f405c6
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-02 20:26:28 +0530

    Add files via upload

---
+++ open py file.py
@@ -0,0 +1,5 @@
+import os
+audiocode=os.startfile("D:\\python in vs\\py projects\\open audio.py")
+
+        # Give the correct directory of the file which u want to open 
+        # Don't forget to write the format ( .py )
---
+++ open video.py
@@ -0,0 +1,13 @@
+import os
+video=os.startfile("D:\\videos\\Rajini intro.mp4")
+
+
+
+
+     # mention the correct directory where u have stored the video in ur system
+     # don't forget to write the file type  ( .mp4 )  
+
+
+
+
+
