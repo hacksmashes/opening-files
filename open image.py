ec43a3d2b7c743878bfbf30621a790e6a592ebca
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-02 20:28:16 +0530

    Update open image.py

--- open image.py
+++ open image.py
@@ -1,10 +1,5 @@
 import os
 img=os.startfile("D:\\python in vs\\py projects\\johncena.jpg")
-img
-{
-    box-shadow: 0,10px,18px black;
-    }
 
         # Give the correct directory of the image which u want to open
-        # Don't forget to write the format  ( .jpg )  or  ( .png )  or  whatever it is . . . plss mention that 
- 
+        # Don't forget to write the format  ( .jpg )  or  ( .png )  or  whatever it is . . . plss mention that
