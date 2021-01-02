1fef9ffb42fe3a24f837b2fe60ceb133cda1fe29
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-02 20:27:19 +0530

    Update open audio.py

--- open audio.py
+++ open audio.py
@@ -1,3 +1,3 @@
 import os
 audio=os.startfile("D:\\songs\\Marvel-Anthem.mp3")          # mention the correct directory where u have stored the songs in ur system
-                                                                                              # don't forget to write the file type  (.mp3)  
+                                                            # don't forget to write the file type  (.mp3)  
